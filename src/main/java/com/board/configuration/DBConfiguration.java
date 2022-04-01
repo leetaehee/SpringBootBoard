5585f07430f8bbffc603589ea14a897ce0e9ab1f
leetaehee <lastride25@naver.com>
2022-04-01 16:01:34 +0900

    aop, transaction

--- src/main/java/com/board/configuration/DBConfiguration.java
+++ src/main/java/com/board/configuration/DBConfiguration.java
@@ -11,12 +11,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
 @PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
+@Configuration
 public class DBConfiguration {
 	
 	@Autowired
@@ -52,5 +56,10 @@ public class DBConfiguration {
 	@ConfigurationProperties(prefix = "mybatis.configuration")
 	public org.apache.ibatis.session.Configuration mybatisConfg() {
 		return new org.apache.ibatis.session.Configuration();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 }
