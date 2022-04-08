b8418dc22f099bf663f1253e875c90040be8eacc
leetaehee <lastride25@naver.com>
2022-04-08 17:34:16 +0900

    검색기능 완료

--- src/main/java/util/UiUtils.java
+++ src/main/java/util/UiUtils.java
@@ -1,10 +1,13 @@
 package util;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.board.paging.Criteria;
 
 import constant.Method;
 
@@ -23,6 +26,18 @@ public class UiUtils {
 		model.addAttribute("params", params);
 		
 		return "utils/message-redirect";
+	}
+	
+	public Map<String, Object> getPagingParams(Criteria criteria) {
+
+		Map<String, Object> params = new LinkedHashMap<>();
+		params.put("currentPageNo", criteria.getCurrentPageNo());
+		params.put("recordsPerPage", criteria.getRecordsPerPage());
+		params.put("pageSize", criteria.getPageSize());
+		params.put("searchType", criteria.getSearchType());
+		params.put("searchKeyword", criteria.getSearchKeyword());
+
+		return params;
 	}
 	
 }
